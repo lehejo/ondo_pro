22abc3ebdac5c8ef11f6b7af606f0662d3b47880
Gayeonng <jkf48239@gmail.com>
2023-04-22 21:41:25 +0900

    연결 성공

--- js folder/gpt.js
+++ js folder/gpt.js
@@ -1,60 +1,50 @@
-window.onload = function() {
-    const chatMessages = document.querySelector('#chat-messages');
-    const userInput = document.querySelector('#user-input');
-    const sendButton = document.querySelector('#send-button');
+window.onload = async function () {
+    const chatMessages = document.querySelector(".chatroom");
     const CHATGPTAPIKEY = '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';
-  
+
+    // 이전에 저장된 날씨 정보 가져오기
+    const weatherData = JSON.parse(localStorage.getItem('weatherData'));
+
+    if (weatherData) {
+        const cityName = weatherData.name;
+        const description = weatherData.weather[0].description;
+        const temperature = weatherData.main.temp;
+
+        const message = `현재 ${cityName}의 날씨는 ${description}, 기온은 ${temperature}℃ 입니다.`;
+        addMessage("Bot", message);
+    }
+
     function addMessage(sender, message) {
-      const messageElement = document.createElement('div');
-      messageElement.className = 'message';
-      messageElement.textContent = sender + ': ' + message;
-      chatMessages.prepend(messageElement);
+        const messageElement = document.createElement('div');
+        messageElement.className = 'message';
+        messageElement.textContent = sender + ': ' + message;
+        chatMessages.prepend(messageElement);
     }
-  
+
     async function fetchATResponse(prompt) {
-      const apiEndpoint = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-      const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${CHATGPTAPIKEY}`
-        },
-        body: JSON.stringify({
-          prompt: prompt,
-          max_tokens: 60,
-          n: 1,
-          stop: '\n',
-        }),
-      };
-  
-      try {
-        const response = await fetch(apiEndpoint, requestOptions);
-        const data = await response.json();
-        const aiResponse = data.choices[0].text.trim();
-        return aiResponse;
-      } catch (error) {
-        console.error('OpenAI API 호출 중 오류 발생:', error);
-        return 'OpenAI API 호출 중 오류 발생';
-      }
+        const apiEndpoint = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${CHATGPTAPIKEY}`
+            },
+            body: JSON.stringify({
+                prompt: prompt,
+                max_tokens: 60,
+                n: 1,
+                stop: '\n',
+            }),
+        };
+
+        try {
+            const response = await fetch(apiEndpoint, requestOptions);
+            const data = await response.json();
+            const aiResponse = data.choices[0].text.trim();
+            return aiResponse;
+        } catch (error) {
+            console.error('OpenAI API 호출 중 오류 발생:', error);
+            return 'OpenAI API 호출 중 오류 발생';
+        }
     }
-  
-    sendButton.addEventListener('click', async () => {
-      const message = userInput.value.trim();
-      if (message.length === 0) return;
-      addMessage('나', message);
-      userInput.value = '';
-      const aiResponse = await fetchATResponse(message);
-      addMessage('챗봇', aiResponse);
-    });
-  
-    userInput.addEventListener('keydown', async (event) => {
-      if (event.key === 'Enter') {
-        const message = userInput.value.trim();
-        if (message.length === 0) return;
-        addMessage('나', message);
-        userInput.value = '';
-        const aiResponse = await fetchATResponse(message);
-        addMessage('챗봇', aiResponse);
-      }
-    });
-  };  
+}
